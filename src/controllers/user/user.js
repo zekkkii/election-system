ed2c8e91950b81f2607f21459f0a613d7b4c5704
zekkkii <evasquezcam@gmail.com>
2021-08-18 16:30:05 -0400

    added user controllers and routes

---
+++ src/controllers/user/user.js
@@ -0,0 +1,56 @@
+const indexUserView = ()=> {
+
+}
+
+const indexUserController = ()=> {
+
+}
+
+const puestosElectivos = ()=> {
+
+}
+
+const puestosPresidenteView = ()=> {
+}
+
+const puestosPresidenteController = ()=> {
+
+}
+
+const puestosAlcaldeView = ()=> {
+
+}
+
+const puestosAlcaldeController = ()=> {
+
+}
+
+const puestosSenadorView = ()=> {
+
+}
+
+const puestosSenadorController = ()=> {
+
+}
+
+const puestosDiputadoView = ()=> {
+
+}
+
+const puestosDiputadoController = ()=> {
+
+}
+
+module.exports = {
+  indexUserView,
+  indexUserController,
+  puestosElectivos,
+  puestosPresidenteView,
+  puestosPresidenteController,
+  puestosAlcaldeView,
+  puestosAlcaldeController,
+  puestosSenadorView,
+  puestosSenadorController,
+  puestosDiputadoView,
+  puestosDiputadoController
+}
--- src/routes/user/user.js
+++ src/routes/user/user.js
@@ -1,5 +1,36 @@
 const { Router } = require('express')
 
+const {
+  indexUserView,
+  indexUserController,
+  puestosElectivos,
+  puestosPresidenteView,
+  puestosPresidenteController,
+  puestosAlcaldeView,
+  puestosAlcaldeController,
+  puestosSenadorView,
+  puestosSenadorController,
+  puestosDiputadoView,
+  puestosDiputadoController
+} = require('../../controllers/user/user')
+
 const router = Router()
+
+router.get('/', indexUserView)
+router.post('/', indexUserController)
+
+router.get('/puestos-electivos', puestosElectivos)
+
+router.get('/catalogo-puestos/Presidente', puestosPresidenteView)
+router.post('/catalogo-puestos/Presidente/:id', puestosPresidenteController)
+
+router.get('/catalogo-puestos/Alcalde', puestosAlcaldeView)
+router.post('/catalogo-puestos/Alcalde/:id', puestosAlcaldeController)
+
+router.get('/catalogo-puestos/Senador', puestosSenadorView)
+router.post('/catalogo-puestos/Senador/:id', puestosSenadorController)
+
+router.get('/catalogo-puestos/Diputado', puestosDiputadoView)
+router.post('/catalogo-puestos/Diputado/:id', puestosDiputadoController)
 
 module.exports = router
